5bad1727a3a5607dd457733df5af4e437f0c2b17
OmarFuadAlnahdi <devumar0@hotmail.com>
2024-09-02 22:04:53 +0300

    Having a problem (The method ClearConsole is undefined for the type mMin Class)

---
+++ First_100/Main.java
@@ -0,0 +1,18 @@
+package First_100;
+import First_100.MehodToUse.*;
+
+
+
+
+public class Main{
+    public static void main(String[] args) {
+        
+        ClearConsole();
+        System.out.println("OutPut:");
+        System.out.println();
+
+        System.out.println("HEllo world");
+        
+         
+    }
+}
---
+++ First_100/MehodToUse/ClearConsole.java
@@ -0,0 +1,9 @@
+package First_100.MehodToUse;
+
+public class ClearConsole {
+    public static void Clear()
+    {
+        System.out.print("\033[H\033[2J");  
+        System.out.flush();
+    };
+}
