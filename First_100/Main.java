16c04715f51ed92afed032e8f4d50db1e1086fc9
OmarFuadAlnahdi <devumar0@hotmail.com>
2024-09-05 08:47:22 +0300

    More Exercise

--- First_100/Main.java
+++ First_100/Main.java
@@ -7,12 +7,25 @@ import First_100.MehodToUse.*;
 public class Main{
     public static void main(String[] args) {
         
-        ClearConsole();
-        System.out.println("OutPut:");
-        System.out.println();
+        ClearConsole console = new ClearConsole();
+        console.Clear(); 
+        
 
-        System.out.println("HEllo world");
+        System.out.println("Hello world");
+        System.out.println("How are you?");
         
          
+
+
+
+
+
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        // this is to make the console clean and readable
+
     }
 }
--- First_100/MehodToUse/ClearConsole.java
+++ First_100/MehodToUse/ClearConsole.java
@@ -1,9 +1,12 @@
 package First_100.MehodToUse;
 
-public class ClearConsole {
+public class  ClearConsole {
+
     public static void Clear()
     {
         System.out.print("\033[H\033[2J");  
         System.out.flush();
+        System.out.println("OutPut:");
+        System.out.println();
     };
 }
